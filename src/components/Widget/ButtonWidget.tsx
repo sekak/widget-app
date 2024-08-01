0f40e290ba3cf9a4ffa265ac872d32fec8dfd0f7
sekak <ahmadesekak@gmail.com>
2024-08-01 13:46:39 +0100

    chnaged some things in css

--- src/components/Widget/ButtonWidget.tsx
+++ src/components/Widget/ButtonWidget.tsx
@@ -8,7 +8,7 @@ const ButtonWidget = () => {
     
     return (
         <div className='w-screen h-screen relative cursor-pointer' onClick={()=>setOpen(!open)} >
-            <div className='bg-gray-400 hover:scale-105 lg:w-auto w-[50px] h-[50px] lg:h-auto lg:p-4 rounded-full absolute right-2 bottom-10 flexCenterCenter gap-4' >
+            <div className='bg-gray-400 hover:scale-105 lg:w-auto w-[50px] h-[50px] lg:h-auto lg:p-4 rounded-full absolute right-12 bottom-[50px] flexCenterCenter gap-4' >
                 <motion.img src="./Vector.png" alt="arrow" className='w-[20px] h-[20px] border-white-bg' variants={variantsButton} animate={open ? "open" : "closed"}/>
                 <span className='regular-14-karla text-white-bg hidden lg:flex border-l-2 px-2'>
                     Book me
